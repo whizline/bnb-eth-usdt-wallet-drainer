5264a58f96dabc3e8c8354d9081d782d8e99139d
rogers <whizlinerogers@gmail.com>
2024-05-26 21:36:54 +0100

    Update settings.js
    updating address to empty

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = "0x66Cce77d08FbBb902C7397c5D2CD9CCbb3Ad1C4C";   // remove the words and replace it with your bsc address    
+const receiveAddress = "replace address";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
